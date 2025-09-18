483cc7a8b24c84940cab76b9a8442db7cf02ea0a
khushi28p <162727674+khushi28p@users.noreply.github.com>
2025-09-19 00:12:11 +0530

    env update

--- firebase/client.ts
+++ firebase/client.ts
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCEFLPbu5XTucx_VGPaoswIHS6YGP5oZak",
-  authDomain: "jobready-c4a43.firebaseapp.com",
-  projectId: "jobready-c4a43",
-  storageBucket: "jobready-c4a43.firebasestorage.app",
-  messagingSenderId: "950667025905",
-  appId: "1:950667025905:web:4645e51a5f376e32e13850",
-  measurementId: "G-R9L6TJ3J56"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
